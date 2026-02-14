bcba65b462cf2e9a8e66089c51d425c44e03b972
SrRonaldRod <ronaldrodri517@gmail.com>
2026-02-13 20:17:01 -0400

    interfaz inicial prueba

--- sistema.c
+++ sistema.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-    printf("hola\n");
+    printf("Proceso de fabricacion\nAlmacen materia prima\nAlmacen producto terminado\nTalento humano\nMercadeo y publicidad\nFinanzas\nProyectos\nMantenimiento\nTecnologia de informacion\nGerencias\nProduccion y calidad\n");
     return 0;
 }
