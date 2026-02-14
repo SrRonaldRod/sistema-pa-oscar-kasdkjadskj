7953ff41c7655ef2ad42b2d9ee2a84f348398097
SrRonaldRod <ronaldrodri517@gmail.com>
2026-02-13 20:19:56 -0400

    interfaz inicial prueba2

--- sistema.c
+++ sistema.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-    printf("Proceso de fabricacion\nAlmacen materia prima\nAlmacen producto terminado\nTalento humano\nMercadeo y publicidad\nFinanzas\nProyectos\nMantenimiento\nTecnologia de informacion\nGerencias\nProduccion y calidad\n");
+    printf("1. Proceso de fabricacion\n2. Almacen materia prima\n3. Almacen producto terminado\n4. Talento humano\n5. Mercadeo y publicidad\n6. Finanzas\n7. Proyectos\n8. Mantenimiento\n9. Tecnologia de informacion\n10. Gerencias\n11. Produccion y calidad\n");
     return 0;
 }
